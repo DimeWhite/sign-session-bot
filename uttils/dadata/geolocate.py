7bd2bcd7c4f89bf7679f60cb5c1c3017c8cee339
DimeWhite <dimsid05@bk.ru>
2025-10-23 00:30:43 +0600

    fix(dadata): add address not found

--- uttils/dadata/geolocate.py
+++ uttils/dadata/geolocate.py
@@ -1,7 +1,9 @@
 from dadata import Dadata
 from config_reader import config
+from typing import Any
 
 def geoLocate(lat: float, lon: float) -> str:
      with Dadata(config.dadata_token.get_secret_value(), config.dadata_secret.get_secret_value(), timeout=1) as dadata:
-          return dadata.geolocate(name="address", lat=lat, lon=lon)[0]["value"]
+          result: list[Any] = dadata.geolocate(name="address", lat=lat, lon=lon)
+          return result[0]["value"] if result else "Не определяется {} {}".format(lat, lon)
           
